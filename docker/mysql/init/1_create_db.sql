55d6f749967c569817f3e436041281e5be4b44a7
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-09-21 15:07:59 +0900

    fix:sql

--- docker/mysql/init/1_create_db.sql
+++ docker/mysql/init/1_create_db.sql
@@ -1,24 +1,36 @@
 CREATE TABLE user(
+  id int(11) auto_increment,
+  name VARCHAR(128) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  email VARCHAR(256) NOT NULL,
+  admin_flag BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (id),
+  UNIQUE KEY (email)
+);
+
+CREATE TABLE trainer(
     id int(11) PRIMARY KEY auto_increment,
-    user_name varchar(255) NOT NULL,
+    user_id int(11),
+    trainer_name varchar(255) NOT NULL,
     winning_count int(11) DEFAULT 0,
     losing_count int(11) DEFAULT 0,
-    battle_flg tinyint(1) NOT NULL DEFAULT false
+    battle_flg tinyint(1) NOT NULL DEFAULT false,
+    FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
 CREATE TABLE pocket_pk(
     id int(11) PRIMARY KEY auto_increment,
-    user_id int(11),
+    trainer_id int(11),
     position int (11),
     pk_id int(11),
-    UNIQUE (user_id, position),
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    UNIQUE (trainer_id, position),
+    FOREIGN KEY(trainer_id) REFERENCES trainer(id)
 );
 
 CREATE TABLE history(
     id int(11) PRIMARY KEY auto_increment,
-    win_user_id int(11),
-    lose_user_id int(11),
-    FOREIGN KEY(win_user_id) REFERENCES user(id),
-    FOREIGN KEY(lose_user_id) REFERENCES user(id)
+    win_trainer_id int(11),
+    lose_trainer_id int(11),
+    FOREIGN KEY(win_trainer_id) REFERENCES trainer(id),
+    FOREIGN KEY(lose_trainer_id) REFERENCES trainer(id)
 );
