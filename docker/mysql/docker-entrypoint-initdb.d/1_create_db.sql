c693ef834a942ba94b7622915ef7258bfa9b5bba
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-10-05 11:57:41 +0900

    mysqlはdockerで動かす

--- docker/mysql/docker-entrypoint-initdb.d/1_create_db.sql
+++ docker/mysql/docker-entrypoint-initdb.d/1_create_db.sql
@@ -1,4 +1,6 @@
-CREATE TABLE user(
+CREATE DATABASE IF NOT EXISTS pokemon;
+
+CREATE TABLE IF NOT EXISTS user(
   id int(11) auto_increment,
   name VARCHAR(128) NOT NULL,
   password VARCHAR(256) NOT NULL,
@@ -8,7 +10,7 @@ CREATE TABLE user(
   UNIQUE KEY (email)
 );
 
-CREATE TABLE trainer(
+CREATE TABLE IF NOT EXISTS trainer(
     id int(11) PRIMARY KEY auto_increment,
     user_id int(11),
     trainer_name varchar(255) NOT NULL,
@@ -18,7 +20,7 @@ CREATE TABLE trainer(
     FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
-CREATE TABLE pocket_pk(
+CREATE TABLE IF NOT EXISTS pocket_pk(
     id int(11) PRIMARY KEY auto_increment,
     trainer_id int(11),
     position int (11),
@@ -27,7 +29,7 @@ CREATE TABLE pocket_pk(
     FOREIGN KEY(trainer_id) REFERENCES trainer(id)
 );
 
-CREATE TABLE history(
+CREATE TABLE IF NOT EXISTS history(
     id int(11) PRIMARY KEY auto_increment,
     win_trainer_id int(11),
     lose_trainer_id int(11),
