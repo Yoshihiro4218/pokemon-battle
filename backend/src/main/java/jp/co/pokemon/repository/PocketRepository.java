22b9703f1f3c59fd1b607f6bc2be2b973d572ceb
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-10-06 21:44:26 +0900

    mybatisのXML対応

--- backend/src/main/java/jp/co/pokemon/repository/PocketRepository.java
+++ backend/src/main/java/jp/co/pokemon/repository/PocketRepository.java
@@ -2,12 +2,10 @@ package jp.co.pokemon.repository;
 
 import jp.co.pokemon.entity.Pocket;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface PocketRepository {
-    @Select("SELECT * FROM pocket_pk WHERE trainer_id = #{trainerId}")
     List<Pocket> findByTrainerId(int trainerId);
 }
--- backend/src/main/java/jp/co/pokemon/repository/TrainerRepository.java
+++ backend/src/main/java/jp/co/pokemon/repository/TrainerRepository.java
@@ -2,12 +2,10 @@ package jp.co.pokemon.repository;
 
 import jp.co.pokemon.entity.Trainer;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface TrainerRepository {
-    @Select("SELECT * FROM trainer WHERE user_id = #{userId}")
     List<Trainer> findByUserId(int userId);
 }
