984339988b8c377b20b98eefe1b76dc2f9bb4384
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-10-06 11:02:40 +0900

    fix

--- backend/src/main/java/jp/co/pokemon/service/impl/UserServiceImpl.java
+++ backend/src/main/java/jp/co/pokemon/service/impl/UserServiceImpl.java
@@ -4,9 +4,7 @@ import jp.co.pokemon.entity.User;
 import jp.co.pokemon.repository.UserRepository;
 import jp.co.pokemon.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
