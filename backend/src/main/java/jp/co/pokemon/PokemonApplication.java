d35170efa695e4c2a1a41c864acffc8e65cc303b
kadono <y.kadono4218@gmail.com>
2019-10-18 15:49:32 +0900

    pom, RedisRepository, ただredisうまくうごかない（Docker）

--- backend/src/main/java/jp/co/pokemon/PokemonApplication.java
+++ backend/src/main/java/jp/co/pokemon/PokemonApplication.java
@@ -2,8 +2,9 @@ package jp.co.pokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { FreeMarkerAutoConfiguration.class })
 public class PokemonApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/jp/co/pokemon/repository/RedisRepository.java
@@ -0,0 +1,64 @@
+package jp.co.pokemon.repository;
+
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.*;
+import org.springframework.stereotype.*;
+
+import java.util.concurrent.*;
+
+@Repository
+public class RedisRepository {
+    private StringRedisTemplate redisTemplate;
+//    private RedisTemplate<String, XxxEntity> redisTemplateSerialize;
+    private HashOperations<String, String, String> hashOps;
+
+    public RedisRepository(StringRedisTemplate stringRedisTemplate,
+                                       RedisConnectionFactory redisConnectionFactory) {
+        this.redisTemplate = stringRedisTemplate;
+//        this.redisTemplateSerialize = pinCodeCredentialRedisTemplate(redisConnectionFactory);
+        this.hashOps = stringRedisTemplate.opsForHash();
+    }
+
+//    public String findPinCode(String key) {
+//        return hashOps.get(key);
+//    }
+
+    public void savePinCode(String emailVerifyToken, String category, String value) {
+        hashOps.put(emailVerifyToken, category, value);
+    }
+
+//    public XxxEntity findPinCodeCredential(String key) {
+//        return (XxxEntity) redisTemplateSerialize.opsForHash().get(key);
+//    }
+
+//    public void savePinCodeCredential(String key, XxxEntity xxxEntity) {
+//        redisTemplateSerialize.opsForHash().put();
+//    }
+
+    private void setExpireTimeLimit(String key) {
+        redisTemplate.expire(key, 500L, TimeUnit.SECONDS);
+    }
+
+//    public void incrementPinCodeLimit(String key) {
+//        hashOps.increment(key, 1L);
+//    }
+
+    public void removeKey(String key) {
+        redisTemplate.delete(key);
+    }
+
+//    private static RedisTemplate<String, XxxEntity> pinCodeCredentialRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<String, XxxEntity> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+//        RedisSerializer<XxxEntity> userInformationRedisSerializer = new Jackson2JsonRedisSerializer<>(XxxEntity.class);
+//        redisTemplate.setKeySerializer(stringSerializer);
+//        redisTemplate.setValueSerializer(userInformationRedisSerializer);
+//        redisTemplate.setHashKeySerializer(stringSerializer);
+//        redisTemplate.setHashValueSerializer(userInformationRedisSerializer);
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
+//    }
+}
+
