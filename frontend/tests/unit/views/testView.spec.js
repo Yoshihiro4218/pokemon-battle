e2ebccf2ac3c8973271b99c14b69f100f2d1f0ef
kadono <y.kadono4218@gmail.com>
2020-04-30 18:46:16 +0900

    [WIP]UT書いたけど、色々問題あり。

---
+++ frontend/tests/unit/views/testView.spec.js
@@ -0,0 +1,35 @@
+import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import TestView from '@/views/TestView';
+
+// jest.mock('@/service/stack.example', () => jest.fn().mockImplementation(() => ({
+//   items: [],
+//   push(input) { this.items.push(input); },
+//   pop: () => 'testPop',
+//   peek: () => 'testPeek',
+//   getCount: () => 123,
+// })));
+
+describe('ExampleStack Page UT', () => {
+  let target = null;
+
+  beforeEach(() => {
+    target = shallowMount(TestView, {});
+    // Vue.filter('moneyDelemiter', function(val){
+    //   return Number(val).toLocaleString(); //3桁区切りでカンマをつける
+    // });
+  });
+
+  // it('inputを入力したとき、データに入力されるか', () => {
+  //   target.find('input').setValue('testText');
+  //
+  //   expect(target.vm.inputData).toEqual('testText');
+  // });
+
+  it('test-price確認', () => {
+    // const actual = target.getElementById('for-test');
+    // expect(actual).toEqual('カイリュー逆鱗発動！');
+
+    expect(1).toEqual(1);
+  });
+});
